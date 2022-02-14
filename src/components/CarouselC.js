90f97449c01f2881e073ef5f0c29ea0b1135fb50
YassineSajjade <yassinesajjade@gmail.com>
2022-02-14 10:50:53 +0100

    fix carousel resolution mobile

--- src/components/CarouselC.js
+++ src/components/CarouselC.js
@@ -14,7 +14,7 @@ function CarouselC() {
                 <div className="carousel-item ">
                     {/* <img src="https://img.freepik.com/photos-gratuite/nettoyage-centre-reparation-automobile-aide-vadrouilles-pour-faire-couler-eau-du-sol-epoxy-dans-centre-service-reparation-automobile_61243-462.jpg?size=626&ext=jpg" className="d-block w-100" alt="..."/> */}
                     <div className='overlay' id='overlay1'></div>
-                    <div className="carousel-caption d-none d-md-block">
+                    <div className="carousel-caption d-block">
                         <h1 style={{ color: '#1190cb'}}>Nettoyage</h1>
                         <p>Société de nettoyage intervenant sur tout le Maroc, avec d'expériences dans le domaine du nettoyage.</p>
                     </div>
@@ -23,7 +23,7 @@ function CarouselC() {
                 <div className="carousel-item active">
                     {/* <img src="https://nobl-business.com/wp-content/uploads/2020/07/s%C3%A9curit%C3%A9-surveillance-et-gardiennage-agadir.jpg" className="d-block w-100" alt="..."/> */}
                     <div className='overlay' id='overlay2'></div>
-                    <div className="carousel-caption d-none d-md-block">
+                    <div className="carousel-caption d-block" id='carousel-caption2'>
                         <h1 style={{ color: 'goldenrod' }}>Gardiennage / Surveillance</h1>
                             <p>Notre Entreprise AS.STONE en surveillance et gardiennage a toujours répondu aux attentes de ses clients en offrant un service de qualité..</p>
                         </div>
@@ -31,7 +31,7 @@ function CarouselC() {
                 <div className="carousel-item ">
                     {/* <img src="https://cdn.verbolia.com/api/images/H4sIAAAAAAAEAJWOsQ6CQBAFf8VsLSyVBZ2NnR3GeuE2x8EeR+4WhBj!XQzG3vIlLzPzhFsUKKFVHVOJ2JghnznWQRzlTfCYQnJ+lBWdJ8sJe14fIRrsKBo3oEaaaVqyfW6PvBstHOFCIjU1!Z90G4IVzsh8HKn4wq60OD!5uzPaQnkqiiOcRTkOpFzxopuh2jsOvw54vQF57Lzq2wAAAA==/travaux-jardinage.jpg" className="d-block w-100" alt="..."/> */}
                     <div className='overlay' id='overlay3'></div>
-                    <div className="carousel-caption d-none d-md-block">
+                    <div className="carousel-caption d-block">
                         <h1 style={{ color: '#6aaf08' }}>Jardinage</h1>
                         <p>Des experts viennent à domicile pour vous aider à réaliser les travaux de jardinage désirés..</p>
                     </div>
