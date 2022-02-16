d4e03749a5c4f674eba2764e9376fee081eeed88
YassineSajjade <yassinesajjade@gmail.com>
2022-02-16 10:44:45 +0100

    Section Services done

--- src/components/SectionServices.js
+++ src/components/SectionServices.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import nettoyage1 from '../images/nettoyage/nettoyage1.jpg'
-import gardiennage1 from '../images/gardiennage/gardiennage1.jpg'
+import nettoyage1 from '../images/nettoyage/nettoyage1.jpg';
+import gardiennage1 from '../images/gardiennage/gardiennage1.jpg';
+import jardinage1 from '../images/jardinage/jardinage1.jpg';
 
 function SectionServices() {
     return (
         <div className='container' style={{ padding: '20px', marginTop: '30px' }}>
 
+            {/* Nettoyage */}
             <div className="card mb-3">
                 <div className="row g-0">
 
@@ -15,13 +17,12 @@ function SectionServices() {
 
                     <div className="col-sm-12 col-md-8 col-lg-8">
                         <div className="card-body">
-                            <h5 className="card-title"><span style={{ color: 'var(--main-color)', fontWeight: 'bold' }}>AS.STONE Maroc</span> Nettoyage</h5>
-                            <p className="card-text lh-base text-break">vous offre un service de nettoyage impeccable à tout moment.<br />
-                                L'entreprise vous offre des services de qualité supérieure et une satisfaction garantie.<br />
-                                Notre societe regroupe des professionnels spécialisés en nettoyage meuble, industriel, tapis, nettoyage de conduits de ventilation, grand ménage et lavage de vitres.<br />
-                                L'Entreprise s'engage à vous offrir un service de qualité à des prix très compétitifs
+                            <h5 className="card-title"><span style={{ color: 'var(--main-color)', fontWeight: 'bold' }}>AS.STONE</span> Nettoyage</h5>
+                            <p className="card-text lh-base text-break">
+                                vous offre un service de nettoyage impeccable à tout moment avec de qualité supérieure et une satisfaction garantie
+                                L'entreprise s'engage à vous offrir un service de qualité à des prix très compétitifs...
                             </p>
-                            <a id='cardText' href='#'>EN SAVOIR +</a>
+                            <a id='cardLink' href='#'>EN SAVOIR +</a>
                         </div>
                     </div>
 
@@ -29,6 +30,7 @@ function SectionServices() {
                 </div>
             </div>
 
+            {/* Gardiennage */}
             <div className="card mb-3">
                 <div className="row g-0">
 
@@ -38,13 +40,11 @@ function SectionServices() {
 
                     <div className="col-sm-12 col-md-8 col-lg-8">
                         <div className="card-body">
-                            <h5 className="card-title"><span style={{ color: 'var(--main-color)', fontWeight: 'bold' }}>AS.STONE Maroc</span> Nettoyage</h5>
-                            <p className="card-text lh-base text-break">vous offre un service de nettoyage impeccable à tout moment.<br />
-                                L'entreprise vous offre des services de qualité supérieure et une satisfaction garantie.<br />
-                                Notre societe regroupe des professionnels spécialisés en nettoyage meuble, industriel, tapis, nettoyage de conduits de ventilation, grand ménage et lavage de vitres.<br />
-                                L'Entreprise s'engage à vous offrir un service de qualité à des prix très compétitifs
+                            <h5 className="card-title"><span style={{ color: 'var(--main-color)', fontWeight: 'bold' }}>AS.STONE</span> Gardiennage</h5>
+                            <p className="card-text lh-base text-break">
+                            Nos sommes particulièrement vigilants à rendre vos biens moins vulnérables. Parce que la sécurité est avant tout une affaire de vigilance humaine...
                             </p>
-                            <a id='cardText' href='#'>EN SAVOIR +</a>
+                            <a id='cardLink' href='#'>EN SAVOIR +</a>
                         </div>
                     </div>
 
@@ -52,22 +52,23 @@ function SectionServices() {
                 </div>
             </div>
 
+            {/* Jardinage */}
             <div className="card mb-3">
                 <div className="row g-0">
 
                     <div className="col-sm-12 col-md-4 col-lg-4 ">
-                        <img src={nettoyage1} className="img-fluid rounded-start float-end" alt="..." />
+                        <img src={jardinage1} className="img-fluid rounded-start float-end" alt="..." />
                     </div>
 
                     <div className="col-sm-12 col-md-8 col-lg-8">
                         <div className="card-body">
-                            <h5 className="card-title"><span style={{ color: 'var(--main-color)', fontWeight: 'bold' }}>AS.STONE Maroc</span> Nettoyage</h5>
-                            <p className="card-text lh-base text-break">vous offre un service de nettoyage impeccable à tout moment.<br />
-                                L'entreprise vous offre des services de qualité supérieure et une satisfaction garantie.<br />
-                                Notre societe regroupe des professionnels spécialisés en nettoyage meuble, industriel, tapis, nettoyage de conduits de ventilation, grand ménage et lavage de vitres.<br />
-                                L'Entreprise s'engage à vous offrir un service de qualité à des prix très compétitifs
+                            <h5 className="card-title"><span style={{ color: 'var(--main-color)', fontWeight: 'bold' }}>AS.STONE</span> Jardinage</h5>
+                            <p className="card-text lh-base text-break">
+                            Nous sommes spécialisés dans la création, l'entretien et l'aménagement des jardins, 
+                            et espaces verts à casablanca et partout au maroc afin de répondre aux besoins des particuliers, 
+                            des syndics de copropriété et des entreprises...
                             </p>
-                            <a id='cardText' href='#'>EN SAVOIR +</a>
+                            <a id='cardLink' href='#'>EN SAVOIR +</a>
                         </div>
                     </div>
 
