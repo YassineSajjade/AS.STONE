f9485d425a959447fd4899a3f30b7d42934dc726
YassineSajjade <yassinesajjade@gmail.com>
2022-03-09 19:10:36 +0100

    fix validation issues

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,18 +8,65 @@ import CopyRight from '../components/CopyRight';
 
 function Contact() {
 
+  const [statusNom, setStatusNom] = useState(false);
+  const [statusSujet, setStatusSujet] = useState(false);
+  const [statusEmail, setStatusEmail] = useState(false);
+  const [statusMessage, setStatusMessage] = useState(false);
+
+  const [nom, setNom] = useState('');
+  const [email, setEmail] = useState('');
+  const [sujet, setSujet] = useState('');
+  const [message, setMessage] = useState('');
+
   const [check, setCheck] = useState('none');
   const [checkNom, setCheckNom] = useState('');
   const [checkEmail, setCheckEmail] = useState('');
   const [checkSujet, setCheckSujet] = useState('');
   const [checkMessage, setCheckMessage] = useState('');
 
+  const handleName = (e) => {
+    if(e.target.value.length < 3){
+      setCheckNom('is-invalid');
+    }else{
+      setCheckNom('is-valid');
+      setNom(e.target.value);
+      setStatusNom(true);
+    }
+  }
+
+  const handleEmail = (e) => {
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if(regex.test(e.target.value) === true){
+      setCheckEmail('is-valid');
+      setEmail(e.target.value);
+      setStatusEmail(true);
+    }else{
+      setCheckEmail('is-invalid');
+    }
+  }
+
+  const handleSujet = (e) => {
+    if(e.target.value.length < 3){
+      setCheckSujet('is-invalid');
+    }else{
+      setCheckSujet('is-valid');
+      setSujet(e.target.value);
+      setStatusSujet(true);
+    }
+  }
+
+  const handleMessqge = (e) => {
+    if(e.target.value.length < 3){
+      setCheckMessage('is-invalid');
+    }else{
+      setCheckMessage('is-valid');
+      setMessage(e.target.value);
+      setStatusMessage(true);
+    }
+  }
+
   const sendEmail = (e) => {
     e.preventDefault();
-    const nom = e.target.nom.value;
-    const email = e.target.email.value;
-    const sujet = e.target.sujet.value;
-    const message = e.target.message.value;
 
     if (nom === '' && email === '' && sujet === '' && message === '') {
       setCheck('block');
@@ -27,22 +74,32 @@ function Contact() {
       setCheckEmail('is-invalid');
       setCheckSujet('is-invalid');
       setCheckMessage('is-invalid');
-    } else {
-      setCheck('none');
-      if (nom.length > 2 ) {
-        setCheckNom('is-valid');
-      }
-      if (email.length > 2 ) {
-        setCheckEmail('is-valid');
-      }
-      if (sujet.length > 2 ) {
-        setCheckSujet('is-valid');
-      }
-      if (message.length > 2 ) {
-        setCheckMessage('is-valid');
+    }else{
+      if(statusNom === true && statusEmail === true && statusSujet === true && statusMessage === true ){
+        resetAll(e);
+        console.log(nom+'/'+email+'/'+sujet+'/'+message);
       }
     }
-    console.log(nom+'/'+email+'/'+sujet+'/'+message)
+  }
+
+  const resetAll = (e) => {
+    setNom('');
+    setEmail('');
+    setSujet('');
+    setMessage('');
+
+    setCheck('none');
+    setCheckNom('');
+    setCheckEmail('');
+    setCheckSujet('');
+    setCheckMessage('');
+
+    setStatusNom(false);
+    setStatusEmail(false);
+    setStatusSujet(false);
+    setStatusMessage(false);
+
+    e.target.reset();
   }
 
   return (
@@ -88,31 +145,31 @@ function Contact() {
                 Ces champs sont obligatoire !!
               </div>
 
-              <form onSubmit={sendEmail}>
+              <form onSubmit={sendEmail} id='from'>
                 <div className="mb-3">
                   <label htmlFor="nom" className="form-label">Nom complet *</label>
-                  <input type="text" className={`form-control ${checkNom} `} id="nom" aria-describedby="nomFeedback" />
+                  <input type="text" className={`form-control ${checkNom} `} id="nom" aria-describedby="nomFeedback" onChange={handleName} />
                   <div id="nomFeedback" className="invalid-feedback">
                     le nom doit étre au moins 3 letters.
                   </div>
                 </div>
                 <div className="mb-3">
                   <label htmlFor="email" className="form-label">Email *</label>
-                  <input type="email" className={`form-control ${checkEmail} `} id="email" aria-describedby='emailFeedback'/>
+                  <input type="text" className={`form-control ${checkEmail} `} id="email" aria-describedby='emailFeedback' onChange={handleEmail}  />
                   <div id="emailFeedback" className="invalid-feedback">
                     le email doit étre valide.
                   </div>
                 </div>
                 <div className="mb-3">
                   <label htmlFor="sujet" className="form-label">Sujet *</label>
-                  <input type="text" className={`form-control ${checkSujet} `} id="sujet" aria-describedby='sujetFeedback'/>
+                  <input type="text" className={`form-control ${checkSujet} `} id="sujet" aria-describedby='sujetFeedback' onChange={handleSujet} />
                   <div id="sujetFeedback" className="invalid-feedback">
                     le sujet doit étre au moins 3 letters.
                   </div>
                 </div>
                 <div className="mb-3">
                   <label htmlFor="message" className="form-label">Message *</label>
-                  <textarea className={`form-control ${checkMessage} `} id="message" rows="3" aria-describedby='messageFeedback'/>
+                  <textarea className={`form-control ${checkMessage} `} id="message" rows="3" aria-describedby='messageFeedback' onChange={handleMessqge}  />
                   <div id="messageFeedback" className="invalid-feedback">
                     le message doit étre au moins 3 letters.
                   </div>
